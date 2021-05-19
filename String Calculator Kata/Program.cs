e148aa4b9d84fe6842c0238c138449097e8e79c2
TheRealDealveloper <66743070+TheRealDealveloper@users.noreply.github.com>
2021-05-19 20:31:05 +0200

    Initial commit

---
+++ String Calculator Kata/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace String_Calculator_Kata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
