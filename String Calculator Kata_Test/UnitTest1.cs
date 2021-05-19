958dd47ef91b5e0a38df9bd5d9d9d99324c852be
TheRealDealveloper <66743070+TheRealDealveloper@users.noreply.github.com>
2021-05-19 20:31:53 +0200

    added testing project

---
+++ String Calculator Kata_Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace String_Calculator_Kata_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
