3d2ad8786f81a42d6e39ad4c1873a2e901c5cf0c
TheRealDealveloper <66743070+TheRealDealveloper@users.noreply.github.com>
2021-05-20 00:47:13 +0200

    Added test for one number

--- String Calculator Kata_Test/UnitTest1.cs
+++ String Calculator Kata_Test/UnitTest1.cs
@@ -8,9 +8,16 @@ namespace String_Calculator_Kata_Test
     {
         [DataTestMethod]
         [DataRow("")]
-        public void Add_Up_Two_Numbers_Returning_(string number)
+        public void Add_Empty_String_Returning_Zero(string number)
         {
             Assert.AreEqual(Program.Add(number), 0);
+        }
+        [DataTestMethod]
+        [DataRow("1")]
+        public void Add_A_Single_Number_As_String_Returning_Number(string number)
+        {
+            int.TryParse(number, out int num);
+            Assert.AreEqual(Program.Add(number), num);
         }
     }
 }
