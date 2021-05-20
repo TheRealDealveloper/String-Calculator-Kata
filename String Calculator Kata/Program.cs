b98460d4185e02b74bd09c50c7669be15373f2ea
TheRealDealveloper <66743070+TheRealDealveloper@users.noreply.github.com>
2021-05-20 08:03:20 +0200

    finished experiment in main method for delimiters with custom length

--- String Calculator Kata/Program.cs
+++ String Calculator Kata/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace String_Calculator_Kata
 {
@@ -6,8 +7,57 @@ namespace String_Calculator_Kata
     {
         public static void Main(string[] args)
         {
-            
-            Console.WriteLine("Hello World");
+            string numbers = "//[***]\n1***2***3***11";
+            int sum = 0;
+            string delimiter;
+            string error = "";
+            if (String.IsNullOrWhiteSpace(numbers))
+            {
+            }
+            else
+            {
+
+                string[] subs = numbers.Split(new Char[] { ',', '\n' });
+                if (subs[0].Contains("//"))
+                {
+                    if (subs[0][2] == '[')
+                    {
+                        var substemp = subs[0].Split(new Char[] { '/', '[', ']' }, StringSplitOptions.RemoveEmptyEntries);
+                        delimiter = substemp[0];
+                        subs = subs[1].Split(delimiter);
+                    }
+                    else
+                    {
+                        delimiter = subs[0][2].ToString();
+                        Console.WriteLine("Delimiter... " + delimiter);
+                        subs = numbers.Split(delimiter);
+                    }
+                    
+                }
+                for (int i = 0; i < subs.Length; i++)
+                {
+                    if (String.IsNullOrWhiteSpace(subs[i]))
+                    {
+                        throw new Exception("Input is NOT ok!");
+                    }
+                    int.TryParse(subs[i], out int num);
+                    if (num < 0)
+                    {
+                        error = error + num + ",";
+                    }
+                    if (num >= 0 && num <= 1000)
+                    {
+                        sum = sum + num;
+                    }
+
+                }
+
+            }
+            if (error != "")
+            {
+                throw new Exception("negatives not allowed: " + error);
+            }
+            Console.WriteLine(sum);
         }
 
         public static int Add(string numbers)
