4b027450c82814e8c39f58a08fbc84682a86b996
TheRealDealveloper <66743070+TheRealDealveloper@users.noreply.github.com>
2021-05-20 07:44:10 +0200

    modified add method to not ignore numbers greater than 1000. Reset main method to default state

--- String Calculator Kata/Program.cs
+++ String Calculator Kata/Program.cs
@@ -6,42 +6,8 @@ namespace String_Calculator_Kata
     {
         public static void Main(string[] args)
         {
-            string numbers = "1,2,3,4,-5,2,-2,-100";
-            int sum = 0;
-            char delimiter;
-            string error = "";
-            if (String.IsNullOrWhiteSpace(numbers))
-            {
-            }
-            else
-            {
-
-                string[] subs = numbers.Split(new Char[] { ',', '\n' });
-                if (subs[0].Contains("//"))
-                {
-                    delimiter = subs[0][2];
-                    subs = numbers.Split(delimiter);
-                }
-                for (int i = 0; i < subs.Length; i++)
-                {
-                    if (String.IsNullOrWhiteSpace(subs[i]))
-                    {
-                        throw new Exception("Input is NOT ok!");
-                    }
-                    int.TryParse(subs[i], out int num);
-                    if (num < 0)
-                    {
-                        error = error + num + ",";
-                    }
-                    sum = sum + num;
-                }
-
-            }
-            if (error != "")
-            {
-                throw new Exception("negatives not allowed: " + error);
-            }
-            Console.WriteLine(sum);
+            
+            Console.WriteLine("Hello World");
         }
 
         public static int Add(string numbers)
@@ -72,7 +38,11 @@ namespace String_Calculator_Kata
                     {
                         error = error + num + ",";
                     }
-                    sum = sum + num;
+                    if (num >= 0 && num <= 1000)
+                    {
+                        sum = sum + num;
+                    }
+                    
                 }
 
             }
