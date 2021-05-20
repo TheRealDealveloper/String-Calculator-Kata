36e249cfc43f53c1b734c527cd16414a795e5f2b
TheRealDealveloper <66743070+TheRealDealveloper@users.noreply.github.com>
2021-05-20 07:32:06 +0200

    main method experiment for debugging reasons

--- String Calculator Kata/Program.cs
+++ String Calculator Kata/Program.cs
@@ -6,14 +6,10 @@ namespace String_Calculator_Kata
     {
         public static void Main(string[] args)
         {
-            
-            Console.WriteLine("Hello World");
-        }
-
-        public static int Add(string numbers)
-        {
+            string numbers = "1,2,3,4,-5,2,-2,-100";
             int sum = 0;
             char delimiter;
+            string error = "";
             if (String.IsNullOrWhiteSpace(numbers))
             {
             }
@@ -33,9 +29,56 @@ namespace String_Calculator_Kata
                         throw new Exception("Input is NOT ok!");
                     }
                     int.TryParse(subs[i], out int num);
+                    if (num < 0)
+                    {
+                        error = error + num + ",";
+                    }
+                    sum = sum + num;
+                }
+
+            }
+            if (error != "")
+            {
+                throw new Exception("negatives not allowed: " + error);
+            }
+            Console.WriteLine(sum);
+        }
+
+        public static int Add(string numbers)
+        {
+            int sum = 0;
+            char delimiter;
+            string error = "";
+            if (String.IsNullOrWhiteSpace(numbers))
+            {
+            }
+            else
+            {
+
+                string[] subs = numbers.Split(new Char[] { ',', '\n' });
+                if (subs[0].Contains("//"))
+                {
+                    delimiter = subs[0][2];
+                    subs = numbers.Split(delimiter);
+                }
+                for (int i = 0; i < subs.Length; i++)
+                {
+                    if (String.IsNullOrWhiteSpace(subs[i]))
+                    {
+                        throw new Exception("Input is NOT ok!");
+                    }
+                    int.TryParse(subs[i], out int num);
+                    if (num < 0)
+                    {
+                        error = error + num + ",";
+                    }
                     sum = sum + num;
                 }
                 
+            }
+            if (error!="")
+            {
+                throw new Exception("negatives not allowed: " + error);
             }
             return sum;
         }
