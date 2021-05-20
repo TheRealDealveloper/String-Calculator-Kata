b93559eacf97817648ed917d93f76fb4953f784d
TheRealDealveloper <66743070+TheRealDealveloper@users.noreply.github.com>
2021-05-20 10:35:06 +0200

    clean up of the main method

--- String Calculator Kata/Program.cs
+++ String Calculator Kata/Program.cs
@@ -8,52 +8,7 @@ namespace String_Calculator_Kata
     {
         public static void Main(string[] args)
         {
-            string numbers = "//[***][%]\n1***2%3";
-            int sum = 0;
-            string delimiter;
-            string error = "";
-            if (String.IsNullOrWhiteSpace(numbers))
-            {
-            }
-            else
-            {
-                string[] subs = numbers.Split(new Char[] { ',', '\n' });
-                if (subs[0].Contains("//"))
-                {
-                    if (subs[0][2] == '[')
-                    {
-                        string[] del = subs[0].Split(new Char[] { '/', '[', ']' }, StringSplitOptions.RemoveEmptyEntries);
-                        var s = subs[1].Split(del, StringSplitOptions.None);
-                        subs = s;
-                    }
-                    else
-                    {
-                        delimiter = subs[0][2].ToString();
-                        subs = numbers.Split(delimiter);
-                    }
-                }
-                for (int i = 0; i < subs.Length; i++)
-                {
-                    if (String.IsNullOrWhiteSpace(subs[i]))
-                    {
-                        throw new Exception("Input is NOT ok!");
-                    }
-                    int.TryParse(subs[i], out int num);
-                    if (num < 0)
-                    {
-                        error = error + num + ",";
-                    }
-                    if (num >= 0 && num <= 1000)
-                    {
-                        sum = sum + num;
-                    }
-                }
-            }
-            if (error != "")
-            {
-                throw new Exception("negatives not allowed: " + error);
-            }
-            Console.WriteLine(sum);
+            Console.WriteLine("Hello World");
         }
 
         public static int Add(string numbers)
